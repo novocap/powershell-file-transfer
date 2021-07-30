d4851db794b10ff728ca2cf0c33663f26a43c3c8
Christian Grimberg <christian.grimberg@outlook.com>
2021-07-30 00:20:18 -0300

    :+1: Add script blocks into script with parameters.

--- Modules/File.Transfer/Public/fileTransfer_UNCPaths.ps1
+++ Modules/File.Transfer/Public/fileTransfer_UNCPaths.ps1
@@ -1,5 +1,4 @@
 [CmdletBinding()]
-
 param (
     [Parameter(Mandatory = $true)]
     [string]
@@ -11,45 +10,41 @@ param (
     [string]
     $FileNameToCopy
 )
-
-if ($null -ne $OriginPath){
-    $originPath = (Get-Item -Path $OriginPath).FullName
-}
-if ($null -ne $DestinationPath){
-    $destinationPath = (Get-Item -Path $DestinationPath).FullName
-}
-if ($null -ne $FileToCopy){
-    $fileNameToCopy = (Get-Item -Path $FileNameToCopy).FullName
-}
-
-$credential = Get-Credential
-
-
-$originSVR = New-PSDrive -Name "OriginSVR" -Root $originPath -PSProvider "FileSystem" -Credential $credential
-
-
-$fileToCopy = Get-ChildItem -Path $originSVR.Root | Where-Object {$_.Name -eq $fileNameToCopy}
-
-
-try {
+{
+    if ($null -ne $OriginPath){
+        $originPath = (Get-Item -Path $OriginPath).FullName
+    }
     
-    Copy-Item -Path $fileToCopy.FullName -Destination $destinationPath
-
-    $fileInDestination = Get-ChildItem -Path $destinationPath | Where-Object {$_.Name -eq $fileToCopy.Name}
-
-    [String] $originFileHash = (Get-FileHash -Path $fileToCopy.FullName -ErrorAction SilentlyContinue).Hash
-    [String] $destinationFileHash = (Get-FileHash -Path $fileInDestination.FullName -ErrorAction SilentlyContinue).Hash
-        
-    if ($originFileHash -eq $destinationFileHash) {
-           Write-Host "OK, the integrity is correct"
+    if ($null -ne $DestinationPath){
+        $destinationPath = (Get-Item -Path $DestinationPath).FullName
     }
-    else {
-         [String] $differentHashError = ("[ERROR] - File integrity error: {0}" -f $DestinationFile)
-         Write-Error -Message $differentHashError -ErrorAction Stop
+    
+    if ($null -ne $FileToCopy){
+        $fileNameToCopy = (Get-Item -Path $FileNameToCopy).FullName
     }
-}
-
-# Catch the error when the file is being used 
-catch [Microsoft.PowerShell.Commands.WriteErrorException] { 
-    Write-Host "Error, the file es being used"
+    
+    $credential = Get-Credential
+    $originSVR = New-PSDrive -Name "OriginSVR" -Root $originPath -PSProvider "FileSystem" -Credential $credential
+    $fileToCopy = Get-ChildItem -Path $originSVR.Root | Where-Object {$_.Name -eq $fileNameToCopy}
+    
+    try {
+        Copy-Item -Path $fileToCopy.FullName -Destination $destinationPath
+    
+        $fileInDestination = Get-ChildItem -Path $destinationPath | Where-Object {$_.Name -eq $fileToCopy.Name}
+    
+        [String] $originFileHash = (Get-FileHash -Path $fileToCopy.FullName -ErrorAction SilentlyContinue).Hash
+        [String] $destinationFileHash = (Get-FileHash -Path $fileInDestination.FullName -ErrorAction SilentlyContinue).Hash
+            
+        if ($originFileHash -eq $destinationFileHash) {
+               Write-Host "OK, the integrity is correct"
+        }
+        else {
+             [String] $differentHashError = ("[ERROR] - File integrity error: {0}" -f $DestinationFile)
+             Write-Error -Message $differentHashError -ErrorAction Stop
+        }
+    }
+    # Catch the error when the file is being used 
+    catch [Microsoft.PowerShell.Commands.WriteErrorException] { 
+        Write-Host "Error, the file es being used"
+    }
 }
