1a70bfe5a6b508be96f8502ac6b1ca500a83e11b
Christian Grimberg <christian.grimberg@outlook.com>
2021-07-31 23:03:10 -0300

    :wrench: Change script to function.

--- Modules/File.Transfer/Public/New-FileTransfer.ps1
+++ Modules/File.Transfer/Public/New-FileTransfer.ps1
@@ -1,43 +1,47 @@
-[CmdletBinding()]
-param (
-    [String]
-    $OriginPath,
-    [String]
-    $DestinationPath,
-    [String]
-    $FileNameToCopy
-)
-{
+function New-FileTransfer {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [String]
+        $OriginPath,
+        [Parameter(Mandatory)]
+        [String]
+        $DestinationPath,
+        [Parameter(Mandatory)]
+        [String]
+        $FileNameToCopy
+    )
+
     if ($null -ne $OriginPath){
         $originPath = (Get-Item -Path $OriginPath).FullName
     }
-    
+
     if ($null -ne $DestinationPath){
         $destinationPath = (Get-Item -Path $DestinationPath).FullName
     }
-    
+
     if ($null -ne $FileToCopy){
         $fileNameToCopy = (Get-Item -Path $FileNameToCopy).FullName
     }
-    
+
     $credential = Get-Credential
     $originSVR = New-PSDrive -Name "OriginSVR" -Root $originPath -PSProvider "FileSystem" -Credential $credential
     $fileToCopy = Get-ChildItem -Path $originSVR.Root | Where-Object {$_.Name -eq $fileNameToCopy}
-    
+
     try {
         Copy-Item -Path $fileToCopy.FullName -Destination $destinationPath
-    
+
         $fileInDestination = Get-ChildItem -Path $destinationPath | Where-Object {$_.Name -eq $fileToCopy.Name}
-    
+
         [String] $originFileHash = (Get-FileHash -Path $fileToCopy.FullName -ErrorAction SilentlyContinue).Hash
         [String] $destinationFileHash = (Get-FileHash -Path $fileInDestination.FullName -ErrorAction SilentlyContinue).Hash
             
         if ($originFileHash -eq $destinationFileHash) {
-               Write-Host "OK, the integrity is correct"
+                Write-Host "OK, the integrity is correct"
         }
         else {
-             [String] $differentHashError = ("[ERROR] - File integrity error: {0}" -f $DestinationFile)
-             Write-Error -Message $differentHashError -ErrorAction Stop
+                [String] $differentHashError = ("[ERROR] - File integrity error: {0}" -f $DestinationFile)
+                Write-Error -Message $differentHashError -ErrorAction Stop
         }
     }
     # Catch the error when the file is being used 
